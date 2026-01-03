1ff9f71ac53ab9d1ff738b5958cd403f541bf49a
cyberider1008 <abaranwal.it@gmail.com>
2026-01-04 00:14:26 +0530

    show product and category

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,42 @@
 import { useEffect , useState} from "react";
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('')
+    fetch('http://127.0.0.1:8000/api/products/')
     .then(response => response.json())
-    .then(data => setMessage(data.message))
+    .then(data => setProducts(data))
     .catch(error => console.error('Error fetching msg', error));
   }, []);
 
 
- return ( 
-  <div>
-    <h1>msg from backend:</h1>
-    <p> {message} </p>
-  </div>
- )
+return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-2xl font-bold mb-4">Product List</h1>
 
-}
+      <ul className="space-y-3">
+        {products.map((item) => (
+          <li
+            key={item.id}
+            className="bg-white p-4 rounded-lg shadow flex justify-between items-center"
+          >
+            <div>
+              <p className="text-sm text-gray-500">ID: {item.id}</p>
+              <p className="text-lg font-semibold">{item.name}</p>
+              <p className="text-gray-600">{item.description}</p>
+            </div>
+
+            <div className="text-right">
+              <p className="text-xl font-bold text-green-600">
+                ₹{item.price}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
